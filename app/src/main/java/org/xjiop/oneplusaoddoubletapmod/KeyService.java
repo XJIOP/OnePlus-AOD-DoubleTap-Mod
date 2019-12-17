6658f5fd20477a7513f8c7d8022226b838ad01bd
XJIOP <xjiopka@gmail.com>
2019-12-17 19:50:21 +0100

    fixed issues with sound volume on lock screen

--- app/src/main/java/org/xjiop/oneplusaoddoubletapmod/KeyService.java
+++ app/src/main/java/org/xjiop/oneplusaoddoubletapmod/KeyService.java
@@ -3,22 +3,36 @@ package org.xjiop.oneplusaoddoubletapmod;
 import android.accessibilityservice.AccessibilityService;
 import android.content.Context;
 import android.os.PowerManager;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
 
 public class KeyService extends AccessibilityService {
 
+    private final String TAG = "KeyService";
+
     private long CLICK_DELAY;
 
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {}
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        //Log.d(TAG, "onAccessibilityEvent: " + event);
+    }
 
     @Override
-    public void onInterrupt() {}
+    public void onInterrupt() {
+        //Log.d(TAG, "onInterrupt");
+    }
 
     @Override
     protected boolean onKeyEvent(KeyEvent event) {
-        return doubleClick();
+        //Log.d(TAG, "onKeyEvent: " + event);
+
+        boolean result = false;
+
+        if(event.getKeyCode() == KeyEvent.KEYCODE_F4)
+            result = doubleClick();
+
+        return result;
     }
 
     private boolean doubleClick() {
@@ -31,15 +45,19 @@ public class KeyService extends AccessibilityService {
             CLICK_DELAY = -1;
             result = true;
 
-            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-            PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK |PowerManager.ACQUIRE_CAUSES_WAKEUP, getPackageName()+":double_tap");
-            wl.acquire(500L);
-            wl.release();
+            PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            if(powerManager != null) {
+                PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, getPackageName() + ":double_tap");
+                wakeLock.acquire(500L);
+                wakeLock.release();
+            }
         }
         else {
             CLICK_DELAY = thisTime;
             result = false;
         }
+
+        //Log.d(TAG, "doubleClick: " + result);
 
         return result;
     }
